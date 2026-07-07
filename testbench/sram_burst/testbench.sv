91c4de6e4b5b81e2863bb82e74a223021841d882
MattGrossi12 <matheusgrossi2000@gmail.com>
2026-07-07 09:08:07 -0300

    Correção da scoreboard do testbench do RTL burst

--- testbench/sram_burst/testbench.sv
+++ testbench/sram_burst/testbench.sv
@@ -20,14 +20,15 @@
         logic [T_AW:0] sram_addr_o;     // Data address output
             
         // Variables for address generation:
-        reg [T_AW:0] addr;
-        reg [T_AW:0] expected_addr;
+        logic [T_AW:0] addr;
+        logic [T_AW:0] expected_addr = 21'bZ;
 
         // Variables for testbench:
-        int temp_state;
-        int test_cases = 100;
-        int error = 0;
-        int passed = 0;
+        logic temp_state;
+        int test_cases = 1000;
+        int error   = 0;
+        int passed  = 0;    
+        int cases   = 0;
 
         // Instantiate the SRAM burst module
         sram_burst dut 
@@ -44,35 +45,33 @@
 
         // Task definitions for testbench operations:
         task rst_task; rst = 1'b0; #10; rst = 1'b1; #10; endtask
-        task div; $display("+----------------------------------------------------------------------------+");endtask
-        task header; div(); $display("| Address Written | adv_ld_n |  Address Expected  | Address Read | Situation |"); div(); endtask
+        task div; $display("+--------------------------------------------------------------------------------------------+");endtask
+        task header; div(); $display("| Address Written | adv_ld_n |  Address Expected  | Address Read |  T_S  | Situation | Cases |"); div(); endtask
         task check_result; div(); $display("          Happen %d errors and %d passed cases                ", error, passed); div(); endtask
 
         // Task to check the output address against the expected address:
         task check; 
             begin
                 //if ((sram_addr_o !== expected_addr) && (sram_adv_ld_n === 1'b0)) 
-                if ((sram_addr_o !== expected_addr)) 
-                    begin
-                        error = error + 1;
-                      	temp_state = 0;
-                    end 
-                else 
-                    begin
-                        passed = passed + 1;
-						temp_state = 1;
-                    end
+                if ((sram_addr_o !== expected_addr))    temp_state = 0;
+                else                                    temp_state = 1;
             end    
         endtask
 
         // Task to display the address and check the output
         task addr_disp;
             begin
+                cases = cases + 1;
                 if(temp_state == 0)
-                  $display("|      %6h     |    %1b     |       %6h       |     %6h   |   ERROR   |", addr, sram_adv_ld_n, expected_addr, sram_addr_o);
+                    begin
+                        error = error + 1;
+                        $display("|      %6h     |    %1b     |       %6h       |     %6h   |   %b   |   ERROR   | %05d |", addr, sram_adv_ld_n, expected_addr, sram_addr_o, temp_state, cases);
+                    end
                 else
-                  $display("|      %6h     |    %1b     |       %6h       |     %6h   |    PASS   |", addr, sram_adv_ld_n, expected_addr, sram_addr_o);
-                check();
+                    begin
+                        passed = passed + 1;
+                        $display("|      %6h     |    %1b     |       %6h       |     %6h   |   %b   |    PASS   | %05d |", addr, sram_adv_ld_n, expected_addr, sram_addr_o, temp_state, cases);
+                    end
             end
         endtask
 
@@ -80,15 +79,16 @@
         task addr_wr;
             begin
                 addr_disp();
+                check();
                 addr = $urandom_range(0, T_AW);
                 sram_addr_i = addr;
                 @(posedge sram_clk)
                     begin
                         sram_adv_ld_n = 1'b1;
-                        #10;
+                        #3;
                         sram_adv_ld_n = 1'b0;
                         expected_addr = addr + 1;
-                        #10;
+                        #3;
                     end
             end
         endtask

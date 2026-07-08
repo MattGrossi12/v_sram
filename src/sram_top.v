011006e136cfc2837539a7050e3b78f8693b7e4a
MattGrossi12 <matheusgrossi2000@gmail.com>
2026-07-08 09:47:45 -0300

    Atualizei o top incluindo o sinal de chip select para dar suporte à multiplos bancos de memórias, nos demais introduzi a lógica miníma necessária, preciso testar ainda, vou fracionar no EDA e testar a versão nova do bank e após validado parto para o teste de topo, por hora não inseri nenhuma mudança relevante no burst, pois de meu ponto de vista pouco muda à ele se for bank A ou bank B, de tal maneira nem introduzi interno à ele essa lógica de banco selecionado, ele irá apenas manipular os endereços

--- src/sram_top.v
+++ src/sram_top.v
@@ -1,23 +1,25 @@
 `timescale 1ns/1ps
 module sram_top #(
     //Widths:
-    parameter DATA_WIDTH = 36,
+    parameter DATA_WIDTH = 18,
     parameter ADDR_WIDTH = 21,
+    parameter BANK_QUANT = 2,
     parameter DATA_DEPTH = 1000000,
 
     //Trully-Widths:
     parameter T_AW   = (ADDR_WIDTH - 1),
     parameter T_DW   = (DATA_WIDTH - 1),
-    parameter T_DD   = (DATA_DEPTH - 1)
+    parameter T_DD   = (DATA_DEPTH - 1),
+    parameter T_BQ   = (BANK_QUANT - 1)
 )(
-	input wire sram_clk,  //Output of sram_sram_clk send of mem_ctrl
-  	input wire rst,  //Assincronous reset
-    input wire [T_AW:0] sram_addr, // Data address
-    input wire sram_we_n,   // 1: read, 0: write
-    input wire sram_oe_n,   // 1: disable, 0: enable
-    input wire sram_adv_ld_n, // 1: disable, 0: enable
-
-  	inout [T_DW:0] sram_data  // Data to write/read
+	  input wire sram_clk,  //Output of sram_sram_clk send of mem_ctrl
+  	input wire rst,                 //Assincronous reset
+    input wire [T_AW:0] sram_addr,  // Data address
+    input wire sram_we_n,           // 1: read, 0: write
+    input wire sram_oe_n,           // 1: disable, 0: enable
+    input wire sram_adv_ld_n,       // 1: disable, 0: enable
+    input wire sram_ce_i, 	        // 1: bankB, 0: bankA
+  	inout [T_DW:0] sram_data        // Data to write/read
 );
 
 wire [T_AW:0] sram_addr_iw = sram_addr;
@@ -45,6 +47,7 @@ assign sram_addr_bus = sram_adv_ld_n ? sram_addr : sram_addr_ow;
                     ) sbk (
                     .sram_clk       (sram_clk),
                     .rst            (rst),  
+                    .sram_ce_i      (sram_ce_i),
                     .sram_addr      (sram_addr_bus),
                     .sram_we_n      (sram_we_n),
                     .sram_oe_n      (sram_oe_n),

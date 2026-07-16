eda1ce64edfcae9f06e26711240e32c728c3ad1c
MattGrossi12 <matheusgrossi2000@gmail.com>
2026-07-16 15:34:55 -0300

    Adicionei o RTL corrigido do Burst, e o testbench do mesmo, parece que agora o comportamento está como esperado

--- testbench/sram_burst/testbench.sv
+++ testbench/sram_burst/testbench.sv
@@ -17,6 +17,7 @@
         logic rst;                      //Assincronous reset
         logic sram_adv_ld_n;            // 1: disable, 0: enable
         logic [T_AW:0] sram_addr_i;     // Data address input
+  		logic sram_we_n ;				// 1: disable, 0: enable
         logic [T_AW:0] sram_addr_o;     // Data address output
             
         // Variables for address generation:
@@ -25,87 +26,132 @@
 
         // Variables for testbench:
         logic temp_state;
-        int test_cases = 10;
+        int test_cases = 30;
         int error   = 0;
         int passed  = 0;    
         int cases   = 0;
         int nw_ad   = 0;
+        int inc_cases  = (test_cases/2);
+        int idle_cases = (test_cases/2);
 
         // Instantiate the SRAM burst module
-        sram_burst dut 
-                        (
-                            .sram_clk(sram_clk),
-                            .rst(rst),
-                            .sram_adv_ld_n(sram_adv_ld_n),
-                            .sram_addr_i(sram_addr_i),
-                            .sram_addr_o(sram_addr_o)
-                        );
+        sram_burst dut (.*);
 
         // Clock generation:
         always #5 sram_clk = ~sram_clk;
 
         // Task definitions for testbench operations:
         task rst_task; rst = 1'b0; #10; rst = 1'b1; #10; endtask
-        task div; $display("+--------------------------------------------------------------------------------------------+");endtask
-        task header; div(); $display("| Address Written | adv_ld_n |  Address Expected  | Address Read |  T_S  | Situation | Cases |"); div(); endtask
+        task div; $display("+--------------------------------------------------------------------------------------+");endtask
+        //task header; div(); $display("| Address Written | adv_ld_n |  Address Expected  | Address Read |  T_S  | Situation | Cases |"); div(); endtask
+        task header; div(); $display("| clk | Address Written | adv_ld_n |  Address Expected  | Address Read |  T_S  | Cases |"); div(); endtask 
         task check_result; div(); $display("          Happen %d errors and %d passed cases                ", error, passed); div(); endtask
-        task IDLE; sram_adv_ld_n = 1; endtask
-        task LOAD; sram_adv_ld_n = 0; endtask
 
-
-        // Task to display the address and check the output
-        task addr_disp;
+        task LOAD_ADDR;
             begin
-                if ((sram_addr_o !== expected_addr)) temp_state = 0; else temp_state = 1;
-                cases = cases + 1;
-                #1;
-                if(temp_state === 0)
-                    begin
-                        error = error + 1;
-                        $display("|      %6h     |    %1b     |       %6h       |     %6h   |   %b   |   ERROR   | %05d |", addr, sram_adv_ld_n, expected_addr, sram_addr_o, temp_state, cases);
-                    end
-                else
-                    begin
-                        passed = passed + 1;
-                        $display("|      %6h     |    %1b     |       %6h       |     %6h   |   %b   |    PASS   | %05d |", addr, sram_adv_ld_n, expected_addr, sram_addr_o, temp_state, cases);
-                    end
+                sram_adv_ld_n   = 1;
+                sram_we_n       = 1;
             end
         endtask
 
-        task addr_wr_op;
+        task SEND_ADDR;
             begin
-                addr_disp();
-                if (nw_ad === 1) nw_addr_wr(); else addr = addr;
-                sram_addr_i = addr;
-                LOAD();
+                sram_adv_ld_n   = 1;
+                sram_we_n       = 0;
+            end
+        endtask
 
-                @(posedge sram_clk)
-                    begin
-                        #7;
-                        IDLE();
-                        expected_addr = addr + 1;
-                        #7;
-                    end
+        task INC;
+            begin
+                sram_adv_ld_n   = 0;
+                sram_we_n       = 1;
+            end
+        endtask
+
+        task IDLE;
+            begin
+                sram_adv_ld_n   = 0;
+                sram_we_n       = 0;
+            end
+        endtask
+
+        // Task to display the address and check the output
+        task disp;
+            begin
+                $monitor("|  %1d  |      %6h     |    %1b     |       %6h       |     %6h   |   %b   | %05d |", sram_clk, sram_addr_i, sram_adv_ld_n, expected_addr, sram_addr_o, temp_state, cases);
             end
         endtask
 
         // Task to write an address and check the output
-        task nw_addr_wr; addr = $urandom_range(0, T_AW); endtask
+        task nw_addr_wr; 
+            begin
+                addr = $urandom_range(0, T_AW);
+                sram_addr_i = addr;
+            end    
+        endtask
+
+        task load_tb; 
+            begin
+                nw_addr_wr(); 
+                expected_addr = addr;
+                LOAD_ADDR(); 
+                #10;
+            end
+        endtask
+
+    task inc_tb;
+        begin
+            @(negedge sram_clk);
+            sram_addr_i = 'z;
+            INC();
+
+            @(posedge sram_clk);
+            #1;
+            expected_addr = expected_addr + 1'b1;
+            cases = cases + 1;
+
+            @(negedge sram_clk);
+            SEND_ADDR();
+
+            @(posedge sram_clk);
+            #1;
+        end
+    endtask
+
+        task mult_inc;
+            begin
+                load_tb();
+                repeat (inc_cases)
+                    begin
+                        inc_tb();
+                    end
+            end
+        endtask
+
+        task idle_tb;
+            begin
+                IDLE();
+                #10;
+            end
+        endtask
+
+        task mult_idle;
+            begin
+                repeat (idle_cases)
+                    begin
+                        idle_tb();
+                    end
+            end
+        endtask
 
         // Main test routine to execute the test cases
         task test_routine;
             begin
                 header();
+                disp();
                 rst_task();
-
-                    //Test with new address of cycle:
-                    nw_ad = 1;
-                    repeat(test_cases) #100 addr_wr_op();
-
-                    //Test improve automatically the address (burst-mode):
-                    nw_ad = 0;
-                    repeat(test_cases) #100 addr_wr_op();
-
+                mult_inc();
+                mult_idle();
                 #500;
                 check_result();
                 $finish;
